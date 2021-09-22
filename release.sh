37030c22f307b4c35d169e9c078aabb781372522
Artemis <QartemisT@gmail.com>
2021-09-22 20:12:23 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -2161,15 +2161,6 @@ if [ -z "$skip_zipfile" ]; then
 	archive_version="$project_version"
 	archive_name="$( filename_filter "$file_name" ).zip"
 	archive_label="$( filename_filter "$file_name" )"
-	if [[ "${file_name}" == *"{game-type}"* ]] || [[ "$game_type" != "retail" && "${file_name}" == *"{classic}"* ]]; then
-		# append the game-type for clarity
-		archive_label="$archive_version-$game_type"
-		if [[ "$game_type" == "classic" && "${project_version,,}" == *"-classic"* ]] || [[ "$game_type" == "bcc" && "${project_version,,}" == *"-bcc"* ]]; then
-			# this is mostly for BigWigs projects that tag classic separately (eg, v10-classic)
-			# to prevent the extra -classic without changing all our workflows
-			archive_label="$archive_version"
-		fi
-	fi
 	archive="$releasedir/$archive_name"
 
 	if [ -n "$GITHUB_ACTIONS" ]; then
